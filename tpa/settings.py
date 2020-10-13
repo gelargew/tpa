c0b66166d6510b1ffbfe4d7f22efbc4ae8a3d881
gelargew <gelargew@gmail.com>
2020-10-14 01:58:29 +0700

    pr

--- tpa/settings.py
+++ tpa/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tes.apps.TesConfig',
-    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- tpa/urls.py
+++ tpa/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from users import views as user_views
 
 
 
