0d5465338ff60433f2317cfbc65a0d546887061f
gelargew <gelargew@gmail.com>
2020-10-14 07:23:39 +0700

    httpresponse

--- tes/views.py
+++ tes/views.py
@@ -136,7 +136,7 @@ def subtest4(request):
 def login(request):
     context = {}
     form = request.POST
-    if len(form) > 4:
+    if form['no_peserta']:
         p = Person(nama=form['name'], nik=form['nik'], no_peserta=form['no_peserta'], jurusan=form['jurusan'])
         if not Person.objects.filter(no_peserta=form['no_peserta']):
             p.save()
